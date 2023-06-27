29eef48215e942a35746c060fc7139e8767140ce
Alex Carolan <alexcarolan@Alexs-MacBook-Pro-2.local>
2023-06-27 16:46:10 +0100

    added ui

--- jetty-test/src/app/app.module.ts
+++ jetty-test/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyAccountComponent } from './my-account/my-account.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
+import { AppHomeComponent } from './components/app-home/app-home.component';
+import { ErrorComponentComponent } from './components/error-component/error-component.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyAccountComponent
+    MyAccountComponent,
+    AppHomeComponent,
+    ErrorComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
