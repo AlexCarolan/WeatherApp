e448c56c9c3ef66a7aec608e24e9963ee37ecbb2
Alex Carolan <alexcarolan@alexs-mbp-2.home>
2023-07-31 16:46:51 +0100

    added unit tests for temp to color directive

--- weather-app/src/app/directives/temperature-color/temperature-color.directive.spec.ts
+++ weather-app/src/app/directives/temperature-color/temperature-color.directive.spec.ts
@@ -1,7 +1,32 @@
+import { ElementRef } from '@angular/core';
 import { TemperatureColorDirective } from './temperature-color.directive';
 
+
 describe('TemperatureColorDirective', () => {
-  it('should create an instance', () => {
-    expect(true).toBeTruthy();
+  let elRef: ElementRef;
+
+  it('generateColor should return cold blue for minus temps', () => {
+    const directive = new TemperatureColorDirective(elRef);
+    expect(directive.generateColor(-1)).toEqual("#2478ff");
+  });
+
+  it('generateColor should return pale blue for low temps', () => {
+    const directive = new TemperatureColorDirective(elRef);
+    expect(directive.generateColor(2.5)).toEqual("#66a1ff");
+  });
+
+  it('generateColor should return pale yellow for mid temps', () => {
+    const directive = new TemperatureColorDirective(elRef);
+    expect(directive.generateColor(16.25)).toEqual("#fae7c5");
+  });
+
+  it('generateColor should return orange for high temps', () => {
+    const directive = new TemperatureColorDirective(elRef);
+    expect(directive.generateColor(27)).toEqual("#f5ab2c");
+  });
+
+  it('generateColor should return red for very high temps', () => {
+    const directive = new TemperatureColorDirective(elRef);
+    expect(directive.generateColor(37)).toEqual("#d43c19");
   });
 });
